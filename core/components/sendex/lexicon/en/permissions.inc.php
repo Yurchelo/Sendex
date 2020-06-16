71036e0532c4ddec23afdb5cd51c38acbda2417c
Yurchelo <66965439+Yurchelo@users.noreply.github.com>
2020-06-16 12:05:03 +0300

    First commit

---
+++ core/components/sendex/lexicon/en/permissions.inc.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Russian permissions Lexicon Entries for Sendex
+ *
+ * @package Sendex
+ * @subpackage lexicon
+ */
+$_lang['sendex_save'] = 'Permission for save/update data.';
---
+++ core/components/sendex/lexicon/ru/permissions.inc.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * English permissions Lexicon Entries for Sendex
+ *
+ * @package Sendex
+ * @subpackage lexicon
+ */
+$_lang['sendex_save'] = 'Разрешает создание/изменение данных.';
